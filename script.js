ed49e53729c3fc96cc3cde9edd55d8fc50c7cd70
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2024-12-04 06:45:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateSizeInfo() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const sizeInfo = document.getElementById('sizeInfo');
+  
+  sizeInfo.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+}
+
+// Initial display
+updateSizeInfo();
+
+// Update size information whenever the window is resized
+window.addEventListener('resize', updateSizeInfo);
